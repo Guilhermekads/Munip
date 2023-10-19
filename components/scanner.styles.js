858b6ff65ae41557d12a2dd6164e2de7e40aac02
Guilhermekads <103399343+Guilhermekads@users.noreply.github.com>
2023-10-19 13:02:18 -0300

    Delete components directory

--- components/scanner.styles.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components/native";
-
-export const ScannerContainer = styled.View`
-    width: 100%;
-    height: 100%;
-    flex-direction: column;
-    justify-content: flex-end;
-`
